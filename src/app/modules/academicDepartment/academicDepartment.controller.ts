849c6867986c4c6078acfad5412c72bd8977f347
nurul287 <nurulalamarif2@gmail.com>
2024-01-26 15:59:33 +0600

    created academic

---
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,66 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicDepartmentServices } from './academicDepartment.service';
+
+const createAcademicDepartment = catchAsync(async (req, res) => {
+  const result =
+    await AcademicDepartmentServices.createAcademicDepartmentIntoDB(req.body);
+
+  sendResponse(res, {
+    success: true,
+    message: 'Academic department is created successfully',
+    data: result,
+    statusCode: httpStatus.OK,
+  });
+});
+
+const getAllAcademicDepartments = catchAsync(async (req, res) => {
+  const result =
+    await AcademicDepartmentServices.getAllAcademicDepartmentsFromDB();
+
+  sendResponse(res, {
+    success: true,
+    message: 'Academic departments are fetched successfully',
+    data: result,
+    statusCode: httpStatus.OK,
+  });
+});
+
+const getSingleAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+  const result =
+    await AcademicDepartmentServices.getSingleAcademicDepartmentFromDB(
+      departmentId,
+    );
+
+  sendResponse(res, {
+    success: true,
+    message: 'Academic department is fetched successfully',
+    data: result,
+    statusCode: httpStatus.OK,
+  });
+});
+
+const updateAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+  const result =
+    await AcademicDepartmentServices.updateAcademicDepartmentIntoDB(
+      departmentId,
+      req.body,
+    );
+
+  sendResponse(res, {
+    success: true,
+    message: 'Academic department is updated successfully',
+    data: result,
+    statusCode: httpStatus.OK,
+  });
+});
+
+export const AcademicDepartmentControllers = {
+  createAcademicDepartment,
+  getAllAcademicDepartments,
+  getSingleAcademicDepartment,
+  updateAcademicDepartment,
+};
---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,38 @@
+import { TAcademicDepartment } from './academicDepartment.interface';
+import { AcademicDepartment } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payLoad: TAcademicDepartment) => {
+  const result = await AcademicDepartment.create(payLoad);
+  return result;
+};
+
+const getAllAcademicDepartmentsFromDB = async () => {
+  const result = await AcademicDepartment.find();
+  return result;
+};
+
+const getSingleAcademicDepartmentFromDB = async (id: string) => {
+  const result = await AcademicDepartment.findById(id);
+  return result;
+};
+
+const updateAcademicDepartmentIntoDB = async (
+  id: string,
+  payLoad: TAcademicDepartment,
+) => {
+  const result = await AcademicDepartment.findOneAndUpdate(
+    { _id: id },
+    payLoad,
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentsFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentIntoDB,
+};
