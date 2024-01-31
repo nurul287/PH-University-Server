a22dd5732773b7275091e82de09c320f8aa14d2f
nurul287 <nurulalamarif2@gmail.com>
2024-02-01 02:37:51 +0600

    update offered course validation.

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+const timeStringSchema = z.string().refine(
+  (time) => {
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format, expected "HH:MM" in 24 hours format',
+  },
+);
+
 const createOfferedCourseValidationSchema = z.object({
   body: z
     .object({
@@ -12,20 +22,8 @@ const createOfferedCourseValidationSchema = z.object({
       maxCapacity: z.number(),
       section: z.number(),
       days: z.array(z.enum([...(Days as [string, ...string[]])])),
-      startTime: z.string().refine(
-        (time) => {
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        { message: 'Invalid time format, expected "HH:MM" in 24 hours format' },
-      ),
-      endTime: z.string().refine(
-        (time) => {
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        { message: 'Invalid time format, expected "HH:MM" in 24 hours format' },
-      ),
+      startTime: timeStringSchema,
+      endTime: timeStringSchema,
     })
     .refine(
       ({ startTime, endTime }) => {
@@ -39,33 +37,11 @@ const createOfferedCourseValidationSchema = z.object({
 
 const updateOfferedCourseValidationSchema = z.object({
   body: z.object({
-    faculty: z.string().optional(),
-    maxCapacity: z.number().optional(),
-    days: z.array(z.enum([...(Days as [string, ...string[]])])).optional(),
-    startTime: z
-      .string()
-      .refine(
-        (time) => {
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format, expected "HH:MM" in 24 hours format',
-        },
-      )
-      .optional(),
-    endTime: z
-      .string()
-      .refine(
-        (time) => {
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format, expected "HH:MM" in 24 hours format',
-        },
-      )
-      .optional(),
+    faculty: z.string(),
+    maxCapacity: z.number(),
+    days: z.array(z.enum([...(Days as [string, ...string[]])])),
+    startTime: timeStringSchema,
+    endTime: timeStringSchema,
   }),
 });
 
