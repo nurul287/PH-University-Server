a037dc3d8ed3b8a724064fd4f37a1cd1cfc49efe
nurul287 <nurulalamarif2@gmail.com>
2025-02-13 18:39:46 +0600

    fix: cookies issue.

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import httpStatus from 'http-status';
-import config from '../../config';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { TAuthUser } from './auth.interface';
@@ -8,9 +7,8 @@ import { AuthServices } from './auth.service';
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUserIntoDB(req.body);
   const { refreshToken, ...data } = result;
-
   res.cookie('refreshToken', refreshToken, {
-    secure: config.node_env === 'production',
+    secure: true,
     httpOnly: true,
     sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24 * 365,
